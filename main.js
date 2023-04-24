df42ec4a0161c40e5e35755ead68628ea52e3b67
Sandeep Sharma <sandeeptech8@gmail.com>
2023-04-24 13:58:13 +0530

    format

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 
-serve((req) => new Response("hello world"), {headers: {"Access-Control-Allow_origin": "*"}});
+serve((req) => new Response("hello world"), {
+  headers: { "Access-Control-Allow_origin": "*" },
+});
 
 // http://localhost:3000
