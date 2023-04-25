492daa51289a0dff14c1f2262bc3a86f0b0395b6
Sandeep Sharma <sandeeptech8@gmail.com>
2023-04-25 15:52:09 +0530

    process auth req

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.post("/auth", (req, res) => {
+  console.log(req.body);
+
+  // todo
+  // 1. get the creds from the body
+  // 2. use the API (client?) to authenticated the given credentials.
+  // note: pass the role along with email and pwd if possible (we won't need to pass relay the request here if this is possible)
+  // 3. match authenticated user role with the supplied role cred
+  // 4. send appropriate response
+
+  res.send({});
 })
 
-app.get("/message", (req, res) => {
-  res.send(req.query.message || "no message");
-})
-
-export { app as hey };
+export { app };
